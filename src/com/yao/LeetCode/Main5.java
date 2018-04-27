d2cfa72f08381927f08e6f461c7ca152d12df1e3
zhuyaoBD <415170029@qq.com>
2018-04-27 21:16:24 +0800

    Some algorithm exercises.

---
+++ src/com/yao/LeetCode/Main5.java
@@ -0,0 +1,37 @@
+package com.yao.LeetCode;
+
+/**
+ * Created by zhuyao on 2018/04/27.
+ */
+public class Main5 {
+    public static String longestPalindrome(String s) {
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+    //从字符串中间向两边扩展比较
+    private static int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+
+    public static void main(String[] args) {
+        String str = "cbbd";
+        String s = longestPalindrome(str);
+        System.out.println(s);
+
+
+    }
+}
