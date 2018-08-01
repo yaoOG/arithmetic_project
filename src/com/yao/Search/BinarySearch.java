88392d13a615b7b91c5266f9206310f38ab66fe1
朱峣 <zhuyao_sx@qiyi.com>
2018-08-01 19:46:19 +0800

    binarySearch

---
+++ src/com/yao/Search/BinarySearch.java
@@ -0,0 +1,33 @@
+package com.yao.Search;
+
+/**
+ * @author zhuyao
+ * @date 2018/08/01
+ */
+public class BinarySearch {
+    public static Integer binarySearch(int[] list, int item) {
+        int low = 0;
+        int high = list.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int guess = list[mid];
+            if (guess == item) {
+                return mid;
+            }
+            if (guess > item) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] myList = {1, 3, 5, 7, 9};
+        System.out.println(binarySearch(myList, 3));
+        System.out.println(binarySearch(myList, -1));
+
+    }
+}
