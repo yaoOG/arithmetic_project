381811d5e2837a49a6d0ed1b160f3fc45375613d
Yao <zhuyaobjtu@yeah.net>
2018-10-29 19:53:31 +0800

    14-18

---
+++ src/com/yao/LeetCode/Main17.java
@@ -0,0 +1,38 @@
+package com.yao.LeetCode;
+
+import java.util.*;
+
+/**
+ * @author zhuyao
+ * @date 2018/10/29
+ * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+ * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+ * 示例:
+ * 输入："23"
+ * 输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ *
+ */
+public class Main17 {
+
+    public static void main(String[] args) {
+        List<String> list = letterCombinations("23");
+        for (String s : list) {
+            System.out.print(s);
+        }
+    }
+    public static List<String> letterCombinations(String digits) {
+        LinkedList<String> ans = new LinkedList<String>();
+        if(digits.isEmpty()) return ans;
+        String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        ans.add("");
+        for(int i =0; i<digits.length();i++){
+            int x = Character.getNumericValue(digits.charAt(i));
+            while(ans.peek().length()==i){
+                String t = ans.remove();
+                for(char s : mapping[x].toCharArray())
+                    ans.add(t+s);
+            }
+        }
+        return ans;
+    }
+}
---
+++ src/com/yao/LeetCode/Main19.java
@@ -0,0 +1,28 @@
+package com.yao.LeetCode;
+
+/**
+ * @author zhuyao
+ * @date 2018/10/29
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ * <p>
+ * 示例：
+ * <p>
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ * <p>
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ * 说明：
+ * <p>
+ * 给定的 n 保证是有效的。
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Main19 {
+
+}
