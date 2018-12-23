815f17c989b161aeaefee984a1eacaef40653c69
zhuyao <415170029@qq.com>
2018-12-23 22:41:04 +0800

    面试题7 重建二叉树

---
+++ src/com/yao/swordRefersOffer/util/BinaryTreeNode.java
@@ -0,0 +1,16 @@
+package com.yao.swordRefersOffer.util;
+
+public class BinaryTreeNode {
+    public int value;
+    public BinaryTreeNode leftNode;
+    public BinaryTreeNode rightNode;
+
+    public BinaryTreeNode(){
+
+    }
+    public BinaryTreeNode(int value){
+        this.value = value ;
+        this.leftNode = null;
+        this.rightNode = null;
+    }
+}
