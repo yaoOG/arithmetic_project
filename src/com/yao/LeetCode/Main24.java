f442a6b640ef81b69c71a7c1600c539eab57582c
Yao <zhuyaobjtu@yeah.net>
2019-02-23 12:27:49 +0800

    addition:Main24-Main29

--- src/com/yao/LeetCode/Main24.java
+++ src/com/yao/LeetCode/Main24.java
@@ -5,6 +5,14 @@ package com.yao.LeetCode;
  * @date 2019/02/20
  */
 public class Main24 {
+    /**
+     * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+     * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+     * 示例:
+     * 给定 1->2->3->4, 你应该返回 2->1->4->3.
+     * @param head
+     * @return
+     */
     public ListNode swapPairs(ListNode head) {
         ListNode dummy = new ListNode(0);
         dummy.next = head;
---
+++ src/com/yao/LeetCode/Main29.java
@@ -0,0 +1,29 @@
+package com.yao.LeetCode;
+
+/**
+ * @author zhuyao
+ * @date 2019/02/22
+ */
+public class Main29 {
+    public int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        long d = Math.abs((long) dividend), b = Math.abs((long) divisor), res = 0;
+        if (1 == b) {
+            return (int) (((dividend < 0) ^ (divisor < 0)) ? d * -1 : d);
+        }
+
+        while (d >= b) {
+            long tmp = b, p = 1;
+            while (d >= (tmp << 1)) {
+                tmp <<= 1;
+                p <<= 1;
+            }
+            d -= tmp;
+            res += p;
+        }
+
+        return (int) (((dividend < 0) ^ (divisor < 0)) ? -res : res);
+    }
+}
