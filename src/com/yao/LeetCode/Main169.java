371209eabac90a4e89190b3faf3c8b5552e6495f
Yao <zhuyaobjtu@yeah.net>
2019-02-26 19:16:24 +0800

    addition:Main169

---
+++ src/com/yao/LeetCode/Main169.java
@@ -0,0 +1,90 @@
+package com.yao.LeetCode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author zhuyao
+ * @date 2019/02/26
+ */
+public class Main169 {
+    /**
+     * 给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+     *
+     * 你可以假设数组是非空的，并且给定的数组总是存在众数。
+     *
+     * 示例 1:
+     *
+     * 输入: [3,2,3]
+     * 输出: 3
+     * 示例 2:
+     *
+     * 输入: [2,2,1,1,1,2,2]
+     * 输出: 2
+     * @param nums
+     * @return
+     */
+    // Sorting
+    public int majorityElement1(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+
+    // Hashtable
+    public int majorityElement2(int[] nums) {
+        Map<Integer, Integer> myMap = new HashMap<>(16);
+        //Hashtable<Integer, Integer> myMap = new Hashtable<Integer, Integer>();
+        int ret = 0;
+        for (int num : nums) {
+            if (!myMap.containsKey(num)) {
+                myMap.put(num, 1);
+            } else {
+                myMap.put(num, myMap.get(num) + 1);
+            }
+            if (myMap.get(num) > nums.length / 2) {
+                ret = num;
+                break;
+            }
+        }
+        return ret;
+    }
+
+    // Moore voting algorithm
+    public int majorityElement3(int[] nums) {
+        int count = 0, ret = 0;
+        for (int num : nums) {
+            if (count == 0)
+                ret = num;
+            if (num != ret)
+                count--;
+            else
+                count++;
+        }
+        return ret;
+    }
+
+    // Bit manipulation
+    public int majorityElement(int[] nums) {
+        int[] bit = new int[32];
+        for (int num : nums)
+            for (int i = 0; i < 32; i++)
+                if ((num >> (31 - i) & 1) == 1)
+                    bit[i]++;
+        int ret = 0;
+        for (int i = 0; i < 32; i++) {
+            bit[i] = bit[i] > nums.length / 2 ? 1 : 0;
+            ret += bit[i] * (1 << (31 - i));
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        Main169 main169 = new Main169();
+        int i = main169.majorityElement2(new int[]{2, 2, 1, 1, 1, 2, 2});
+        System.out.println(i);
+
+
+
+    }
+}
