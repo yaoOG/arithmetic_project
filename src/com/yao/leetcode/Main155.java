63e928875c6bd0e79eb158d83acc62d0770242a8
Daniel <danielchuyao@gmail.com>
2021-06-08 22:19:10 +0800

    add solution

--- src/com/yao/leetcode/Main155.java
+++ src/com/yao/leetcode/Main155.java
@@ -33,6 +33,18 @@ import java.util.Stack;
  * minStack.getMin();   --> 返回 -2.
  */
 public class Main155 {
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        minStack.getMin();
+        minStack.pop();
+        minStack.top();
+        minStack.getMin();
+
+
+    }
 
 }
 
